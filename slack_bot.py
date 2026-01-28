881298f91ea26fdf566ea703d7f79cde2d1bd53a
Manika Dahal <manikadahal8@gmail.com>
2026-01-28 11:12:10 +0545

    Fixed code for github action

--- slack_bot.py
+++ slack_bot.py
@@ -1,29 +1,23 @@
 import os
-import time
-import schedule
 from slack_sdk import WebClient
-from datetime import datetime
 
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 SLACK_CHANNEL_ID = os.environ.get("SLACK_CHANNEL_ID")
 
 if not SLACK_BOT_TOKEN or not SLACK_CHANNEL_ID:
-    raise ValueError("Slack token or channel ID not set")
+    raise ValueError("Slack token or channel ID not set!")
 
 client = WebClient(token=SLACK_BOT_TOKEN)
 
 def send_reminder():
-    print("Sending reminder at:", datetime.now())
-    client.chat_postMessage(
-        channel=SLACK_CHANNEL_ID,
-        text="8 PM Reminder!\n1. GitHub ma `git pull` ra `git push` garna nabirsinu\n2. Output Emulator side by side rakhera garnus hai"
-    )
+    try:
+        client.chat_postMessage(
+            channel=SLACK_CHANNEL_ID,
+            text="8 PM Reminder!\n1. GitHub ma `git pull` ra `git push` garna nabirsinu \n2. Output Emulator side by side rakhera garnus hai"
+        )
+        print("Reminder sent!")
+    except Exception as e:
+        print("Failed to send reminder:", e)
 
-# Nepal time is server time independent
-schedule.every().day.at("20:00").do(send_reminder)
-
-print("Slack bot started. Waiting for 8 PM...")
-
-while True:
-    schedule.run_pending()
-    time.sleep(30)
+if __name__ == "__main__":
+    send_reminder()
